fb834513505d5bd12ecb9435f262395c483178c0
Brandon <brandoncasa8@gmail.com>
2021-06-15 09:02:06 -0400

    Did navbar stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <div className="AppGridContainer">
-
+        <div className="Navbar">
+          <div className="NavbarWaffle">
+            <i className="material-icons-round NavbarWaffleIcon">menu</i>
+          </div>
+          <div className="NavbarHome">
+            <i className="material-icons-round NavbarHomeIcon">home</i>
+          </div>
+        </div>
+        <div className="AppGridItem"></div>
       </div>
     </div>
   );
