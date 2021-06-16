69c0ea9f867c5c522a0721998190d01b5d67ff71
Brandon <brandoncasa8@gmail.com>
2021-06-16 12:01:24 -0400

    Server buttons

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
+import React from "react";
 import "./App.scss";
+import ServerIcon from "./components/ServerIcon";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="AppGridContainer">
-        <div className="Serverbar"></div>
-        <div className="Navbar">
-          <div className="NavbarHome NavbarCircleBorder noselect">
-            <i className="material-icons-round NavbarIcon noselect">home</i>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="AppGridContainer">
+          <div className="Serverbar">
+            <div className="Servers">
+              <ServerIcon />
+              <ServerIcon />
+              <ServerIcon />
+            </div>
           </div>
-          <div className="NavbarAccountSettings noselect">
-            <i className="material-icons-round NavbarIcon noselect">manage_accounts</i>
+          <div className="Navbar">
+            <div className="NavbarHome NavbarCircleBorder noselect">
+              <i className="material-icons-round NavbarIcon noselect">home</i>
+            </div>
+            <div className="NavbarAccountSettings noselect">
+              <i className="material-icons-round NavbarIcon noselect">manage_accounts</i>
+            </div>
+            <div className="NavbarLightMode noselect">
+              <i className="material-icons-round NavbarIcon noselect">light_mode</i>
+            </div>
+            <div className="NavbarSettings noselect">
+              <i className="material-icons-round NavbarIcon noselect">settings</i>
+            </div>
           </div>
-          <div className="NavbarLightMode noselect">
-            <i className="material-icons-round NavbarIcon noselect">light_mode</i>
-          </div>
-          <div className="NavbarSettings noselect">
-            <i className="material-icons-round NavbarIcon noselect">settings</i>
-          </div>
+          <div className="AppGridItem"></div>
         </div>
-        <div className="AppGridItem"></div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
