189058929836c2c077130d56d85a001f069a913e
Brandon <brandoncasa8@gmail.com>
2021-06-16 09:38:29 -0400

    Button stuff
    - Added button animation's such as hover and active
    - Added more buttons
    ^ all about the navbar buttons ^

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ function App() {
     <div className="App">
       <div className="AppGridContainer">
         <div className="Navbar">
-          <div className="NavbarWaffle">
-            <i className="material-icons-round NavbarWaffleIcon">menu</i>
+          <div className="NavbarHome NavbarCircleBorder noselect">
+            <i className="material-icons-round NavbarIcon noselect">home</i>
           </div>
-          <div className="NavbarHome">
-            <i className="material-icons-round NavbarHomeIcon">home</i>
+          <div className="NavbarAccountSettings noselect">
+            <i className="material-icons-round NavbarIcon noselect">manage_accounts</i>
+          </div>
+          <div className="NavbarLightMode noselect">
+            <i className="material-icons-round NavbarIcon noselect">light_mode</i>
+          </div>
+          <div className="NavbarSettings noselect">
+            <i className="material-icons-round NavbarIcon noselect">settings</i>
           </div>
         </div>
         <div className="AppGridItem"></div>
