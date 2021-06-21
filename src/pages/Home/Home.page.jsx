9c38023c77b581179d892dc8c59d1ade8f4569fb
Brandon <brandoncasa8@gmail.com>
2021-06-21 02:59:05 -0400

    Stuff

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -6,8 +6,16 @@ class HomePage extends React.Component {
     return (
       <div className="Home">
         <div className="maincontent">
-          <div className="notificationbox">notifications</div>
-          <div className="friendsbox">friends</div>
+          <div className="notificationbox">
+            <div className="title">Notifications</div>
+            <hr class="solidborder" />
+          </div>
+          <div className="friendsbox">
+            <div className="title">Friend Activity</div>
+            <div>
+              <hr class="solidborder" />
+            </div>
+          </div>
         </div>
       </div>
     );
