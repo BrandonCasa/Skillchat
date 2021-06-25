fb8ad394fb88e9fa8b9ddde96c67f612716fd6eb
Brandon <brandoncasa8@gmail.com>
2021-06-25 17:38:40 -0400

    Fixed video

--- src/pages/Landing/Landing.page.jsx
+++ src/pages/Landing/Landing.page.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Landing.page.scss";
 import vid from "./background.webm";
+import steam from "./background.webm";
 
 function LandingPage() {
   return (
     <div className="Landing">
       <div className="maincontent">
-        <div className="video">
-          <div className="title"></div>
+        <div className="videocont">
           <video src={vid} width="100%" height="100%" type="video/x-webm" autoPlay muted loop></video>
-          <div className="chevrons"></div>
+          <video src={vid} width="100%" height="100%" type="video/x-webm" autoPlay muted loop></video>
         </div>
         <div className="information"></div>
       </div>
