223078702f262f4859377c7b58f8a55e19876df8
Brandon <brandoncasa8@gmail.com>
2021-06-25 18:00:03 -0400

    Fixed all video issues

--- src/pages/Landing/Landing.page.jsx
+++ src/pages/Landing/Landing.page.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Landing.page.scss";
 import vid from "./background.webm";
-import steam from "./background.webm";
+import steam from "./steam.webm";
 
 function LandingPage() {
+  const [vidVis, setVidVis] = useState(false);
+
+  const changeVal = () => {
+    setVidVis(true);
+    document.getElementById("steamvid").play();
+  };
+  setTimeout(changeVal, 500);
+
   return (
     <div className="Landing">
       <div className="maincontent">
         <div className="videocont">
-          <video src={vid} width="100%" height="100%" type="video/x-webm" autoPlay muted loop></video>
-          <video src={vid} width="100%" height="100%" type="video/x-webm" autoPlay muted loop></video>
+          <video src={vid} width="100%" height="100%" type="video/x-webm" autoPlay muted loop style={vidVis ? {} : { display: "none" }}></video>
+          <video id="steamvid" src={steam} width="100%" height="100%" muted></video>
         </div>
         <div className="information"></div>
       </div>
