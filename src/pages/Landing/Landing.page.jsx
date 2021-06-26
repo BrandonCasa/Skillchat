b2614c1165cc697cbf214c41084ca80f266d4edb
Brandon <brandoncasa8@gmail.com>
2021-06-26 11:33:06 -0400

    New landing page

--- src/pages/Landing/Landing.page.jsx
+++ src/pages/Landing/Landing.page.jsx
@@ -1,28 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Landing.page.scss";
-import vid from "./background.webm";
-import steam from "./steam.webm";
 
 function LandingPage() {
-  const [vidVis, setVidVis] = useState(false);
-
-  const changeVal = () => {
-    setVidVis(true);
-    document.getElementById("steamvid").play();
-  };
-  setTimeout(changeVal, 500);
-
   return (
     <div className="Landing">
       <div className="maincontent">
-        <div className="videocont">
-          <video src={vid} width="100%" height="100%" type="video/x-webm" autoPlay muted loop style={vidVis ? {} : { display: "none" }}></video>
-          <video id="steamvid" src={steam} width="100%" height="100%" muted></video>
-          <div className="overlay">
-            <div className="title">Please login to enjoy social features, more information below.</div>
-          </div>
+        <div className="title">
+          <div className="primary">The eSports chat to replace Discord and Guilded</div>
+          <div className="secondary">This unique community based application allows eSports teams to coordinate and work effectively with each of their members' schedules.</div>
+          <div className="tertiary">Please login with Google to continue.</div>
         </div>
-        <div className="information">xd no info</div>
       </div>
     </div>
   );
